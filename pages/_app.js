7f9aee5b061af189e0b1d8499e7b349881d1e535
mahbubhswe <mahbubhswe@gmail.com>
2022-10-02 23:21:13 +0600

    set dynamic text

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,7 @@ import { CacheProvider } from "@emotion/react";
 import theme from "../src/theme";
 import createEmotionCache from "../src/createEmotionCache";
 const clientSideEmotionCache = createEmotionCache();
-
+import Marquee from "react-fast-marquee";
 export default function MyApp(props) {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
 
@@ -18,9 +18,19 @@ export default function MyApp(props) {
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
       <ThemeProvider theme={theme}>
-
         <CssBaseline />
-        <Component {...pageProps} />
+        <div>
+          <Marquee
+            gradient={false}
+            speed={30}
+            style={{ width: "80%", margin: "auto", marginTop: "20px" }}
+          >
+            আপনার ডাটা আমাদের কাছে সম্পুর্ন ভাবে নিরাপদে থাকবে। তাই, আপনি কোন
+            প্রকার দুশ্চিন্তা না করে আজ থেকে এখানেই মিলের সকল হিসাব রাখুন।
+             এটি সম্পুর্ন ফ্রি!
+          </Marquee>
+          <Component {...pageProps} />
+        </div>
       </ThemeProvider>
     </CacheProvider>
   );
