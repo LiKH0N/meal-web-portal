14003fdc9c13cf24edb44236cbcad64b4f8f9fa5
mahbubhswe <mahbubhswe@gmail.com>
2022-10-03 02:23:56 +0600

    fixed design

--- pages/check-meal.js
+++ pages/check-meal.js
@@ -1,10 +1,27 @@
+import { Container } from "@mui/material";
+import Head from "next/head";
 import React from "react";
 import CheckMealForm from "../components/CheckMealForm";
-import Layout from "../layout/Layout";
 export default function Index() {
   return (
-    <Layout pageTitle="Check your meal">
-      <CheckMealForm />
-    </Layout>
+    <React.Fragment>
+      <Head>
+        <title>Meal checking system</title>
+      </Head>
+      <Container
+        maxWidth="md"
+        sx={{
+          background: "#F7F8F9",
+          p: "10px",
+          borderRadius: "10px",
+          borderTop: "3px solid #FF9D14",
+          border: "1px soldi #ccc",
+          boxShadow: "0 1px 4px 0 rgb(0 0 0 / 50%)",
+          my: "50px",
+        }}
+      >
+        <CheckMealForm />
+      </Container>
+    </React.Fragment>
   );
 }
